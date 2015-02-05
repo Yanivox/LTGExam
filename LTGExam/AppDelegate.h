726a2e1068dd7f70d7f77210cf63dbbdd66ae439
Yaniv Marshaly <y.marshaly@gmail.com>
2015-02-05 16:51:19 +0200

    Initial Commit

---
+++ LTGExam/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  LTGExam
+//
+//  Created by Yaniv Marshaly on 2/5/15.
+//  Copyright (c) 2015 Yaniv Marshaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
